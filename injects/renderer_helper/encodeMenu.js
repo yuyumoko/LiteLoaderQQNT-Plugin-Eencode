84d556c59ea3b37e9bdfa48edb21df7612a2f37a
erinilis <389897773@qq.com>
2023-09-15 21:25:26 +0800

    在菜单中添加解密文件按钮

--- injects/renderer_helper/encodeMenu.js
+++ injects/renderer_helper/encodeMenu.js
@@ -15,6 +15,23 @@
 </div>
 <!----><span class="q-context-menu-item__text">加密选中</span><!---->
 `;
+  
+const decryptFileHtml = `
+<div class="q-context-menu-item__icon q-context-menu-item__head"><i class="q-icon vue-component" data-v-717ec976=""
+        style="--b4589f60: inherit; --6ef2e80d: 16px;">
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+            stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+            class="feather feather-codepen">
+            <polygon points="12 2 22 8.5 22 15.5 12 22 2 15.5 2 8.5 12 2"></polygon>
+            <line x1="12" y1="22" x2="12" y2="15.5"></line>
+            <polyline points="22 8.5 12 15.5 2 8.5"></polyline>
+            <polyline points="2 15.5 12 8.5 22 15.5"></polyline>
+            <line x1="12" y1="2" x2="12" y2="8.5"></line>
+        </svg>
+    </i>
+</div>
+<!----><span class="q-context-menu-item__text">解密文件</span><!---->
+`;
   return (event, isImageViewer = false) => {
     var hasFound = false;
     var timer = setInterval(async () => {
@@ -44,17 +61,25 @@
         return;
       }
 
-      const qMenu = document.createElement("a");
-      qMenu.classList.add(
-        "q-context-menu-item",
-        "q-context-menu-item--normal",
-        "encode-select-msg"
-      );
-      qMenu.setAttribute("aria-disabled", "false");
-      qMenu.setAttribute("role", "menuitem");
-      qMenu.setAttribute("tabindex", "-1");
-      qMenu.innerHTML = menuHtml;
+      function createMenu(html) {
+        const qMenu = document.createElement("a");
+        qMenu.classList.add(
+          "q-context-menu-item",
+          "q-context-menu-item--normal",
+          "encode-select-msg"
+        );
+        qMenu.setAttribute("aria-disabled", "false");
+        qMenu.setAttribute("role", "menuitem");
+        qMenu.setAttribute("tabindex", "-1");
+        qMenu.innerHTML = html;
+        return qMenu;
+      }
+
+      const qMenu = createMenu(menuHtml);
       qContextMenu.appendChild(qMenu);
+
+      const decryptFileMenu = createMenu(decryptFileHtml);
+      qContextMenu.appendChild(decryptFileMenu);
 
       const rect = qContextMenu.getBoundingClientRect();
       if (rect.bottom > window.innerHeight) {
@@ -141,6 +166,27 @@
           copyTextToClipboard(text);
         }
       });
+
+      // 解密文件事件
+      decryptFileMenu.addEventListener("click", async () => {
+        qContextMenu.remove();
+        const fileElement = findParent(event.target, "file-element");
+        if (fileElement) {
+          const fileInfo = fileElement.__VUE__[0].props.msgElement.fileElement;
+          const filePath = fileInfo.filePath;
+          const isExist = await eencode.existsSync(filePath);
+          // console.log(fileInfo);
+          if (!isExist) {
+            showMsg("找不到加密文件, 请重新下载", "error");
+            return;
+          }
+          await eencode.DecryptFile(filePath);
+          await eencode.deleteFileSync(filePath);
+          showMsg("文件解密成功!");
+        }
+      });
+
+
     }, 50);
   };
 };
